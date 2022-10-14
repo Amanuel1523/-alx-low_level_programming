249cad1401800da0c0cb7b919adee56f366a95c4
Amanuel Mulugeta <amanuel@example.com>
2022-10-14 01:42:54 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,10 @@
  * starts
  * here
  */
-int main (void)
+int main(void)
 {
-	char str[] = "Programming is like building a multilingual puzzle";
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(str);
 	return (0);
 }
