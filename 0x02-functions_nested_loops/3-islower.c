c07cd50844880d1d44c4a5231f68d7cd8e79327b
Amanuel Mulugeta <amanuel@example.com>
2022-10-19 06:26:11 -0700

    _islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,6 +2,8 @@
 /**
  * _islower - check ethier the input is lowercase or not
  *
+ * c - is the parameter of the defined function _islower
+ *
  * Return: if it returns 1 the letter is in lowercase else it returns 0
  */
 int _islower(int c)
